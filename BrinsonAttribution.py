1482b518bfc805cec8ff6b1bf1c96b6f2c00d3cb
CUFEWGS <wanggs94@outlook.com>
2022-01-21 10:50:26 +0800

    some change

---
+++ BrinsonAttribution.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/1/20 16:44
+# @Author   ：Wang Guosong
+# @File     : BrinsonAttribution.py
+# @Software : PyCharm
+
+import pandas as pd
+
+class BrinsonAttribution:
+
+    @staticmethod
+    def cal_sr(bm_weight, pf_sector_ret, bm_sector_ret):
+        """
+
+        Parameters
+        ----------
+        bm_weight
+        pf_sector_ret
+        bm_sector_ret
+
+        Returns
+        -------
+        Examples
+        -------
+        >>> bm_weight = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> pf_sector_ret = pd.Series([0.11, 0.19, 0.32, 0.14, 0.09],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> bm_sector_ret = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> bm_sector_ret = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['aa', 'b', 'c', 'd', 'e'])
+        >>> BrinsonAttribution.cal_sr(bm_weight, pf_sector_ret, bm_sector_ret)
+        """
+        if not((bm_weight.index == pf_sector_ret.index).all() and (pf_sector_ret.index == bm_sector_ret.index).all()):
+            raise ValueError("Benchmark and portfolio must have the same index with value of sectors.")
+        sr = bm_weight * (pf_sector_ret - bm_sector_ret)
+        return sr
+
+    @staticmethod
+    def cal_sr_ir(pf_weight, pf_sector_ret, bm_sector_ret):
+        """
+
+        Parameters
+        ----------
+        pf_weight
+        pf_sector_ret
+        bm_sector_ret
+
+        Returns
+        -------
+        Examples
+        -------
+        >>> bm_weight = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> pf_sector_ret = pd.Series([0.11, 0.19, 0.32, 0.14, 0.09],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> bm_sector_ret = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['a', 'b', 'c', 'd', 'e'])
+        >>> bm_sector_ret = pd.Series([0.1, 0.2, 0.3, 0.15, 0.05],
+        index=['aa', 'b', 'c', 'd', 'e'])
+        >>> BrinsonAttribution.cal_sr(bm_weight, pf_sector_ret, bm_sector_ret)
+        """
+        if not ((pf_weight.index == pf_sector_ret.index).all() and (pf_sector_ret.index == bm_sector_ret.index).all()):
+            raise ValueError("Benchmark and portfolio must have the same index with value of sectors.")
+        sr_ir = pf_weight * (pf_sector_ret - bm_sector_ret)
+        return sr_ir
+
+    @staticmethod
+    def cal_ar(pf_weight, bm_weight, bm_sector_ret, bm_index_ret=0):
+        if not ((pf_weight.index == bm_sector_ret.index).all and (bm_weight.index == bm_sector_ret.index).all()):
+            raise ValueError("Benchmark and portfolio must have the same index with value of sectors.")
+        ar = (pf_weight - bm_weight) * (bm_sector_ret - bm_index_ret)
+        return ar
+
+    @staticmethod
+    def cal_ir(pf_weight, bm_weight, pf_sector_ret, bm_sector_ret):
+        if not ((pf_weight.index == bm_sector_ret.index).all and (bm_weight.index == pf_sector_ret.index).all() and\
+                (pf_sector_ret.index == bm_sector_ret.index).all()):
+            raise ValueError("Benchmark and portfolio must have the same index with value of sectors.")
+        ir = (pf_weight - bm_weight) * (pf_sector_ret - bm_sector_ret)
+        return ir
+
+    @staticmethod
+    def cal_sector_ret(stock_ret, stock_weight_in_bm, stock_sector):
+        """
+
+        Parameters
+        ----------
+        stock_ret
+        stock_weight_in_bm
+        stock_sector
+
+        Returns
+        -------
+
+        Examples
+        -------
+        >>> import pandas as pd
+        >>> weight_dir = "D:/E/Python/Brinson-Attribution/index_weight/000300.csv"
+        >>> stock_weight_in_bm = pd.read_csv(weight_dir, encoding='utf-8')
+        >>> stock_weight_in_bm  = stock_weight_in_bm[['Unnamed: 0', '2017-12-29']]
+        >>> stock_weight_in_bm.set_index('Unnamed: 0', inplace=True)
+        >>> sector_dir = "D:/E/Python/Brinson-Attribution/quote_data/industry_zx.csv"
+        >>> stock_sector = pd.read_csv(sector_dir, encoding='gbk')[['Unnamed: 0', '2017/12/29']]
+        >>> stock_sector.set_index('Unnamed: 0', inplace=True)
+        >>> stock_ret = pd.read_csv("D:/E/Python/Brinson-Attribution/quote_data/pct_chg_6M.csv")[['Unnamed: 0', '2017/12/31']]
+        >>> stock_ret.set_index('Unnamed: 0', inplace=True)
+        """
+        stock_weight_in_bm = stock_weight_in_bm.dropna()
+        stock_sector = stock_sector.dropna()
+        stock_ret = stock_ret.dropna()
+        multi_index = stock_weight_in_bm.index.intersection(stock_sector.index).intersection(stock_ret.index)
+        stock_weight_in_bm = stock_weight_in_bm.reindex(multi_index)
+        stock_sector = stock_sector.reindex(multi_index)
+        stock_ret = stock_ret.reindex(multi_index)
+        pd.concat
+        bm_sector_return = (stock_weight_in_bm.iloc[:, 0] * stock_ret.iloc[:, 0]) / stock_weight_in_bm.iloc[:, 0].sum()
+        pass
+
+
--- brinson_attribution.py
+++ brinson_attribution.py
@@ -10,11 +10,30 @@ import numpy as np
 import pandas as pd
 from itertools import dropwhile
 import warnings
-warnings.filterwarnings('ignore') 
+from wm import DateUtil
 
-wdir = os.path.dirname(__file__)
+warnings.filterwarnings('ignore')
+
+# wdir = os.path.dirname(__file__)
+wdir = "D:/E/Python/Brinson-Attribution"
+
 
 def get_ind_data(weight, ind_type='zx'):
+    """
+
+    Parameters
+    ----------
+    weight
+    ind_type
+
+    Returns
+    -------
+
+    Examples
+    ------
+    >>>
+
+    """
     industry_dat = pd.read_csv(os.path.join(wdir, 'quote_data', f'industry_{ind_type}.csv'),
                                encoding='gbk', engine='python', index_col=[0])
     industry_dat.columns = pd.to_datetime(industry_dat.columns)
@@ -23,18 +42,15 @@ def get_ind_data(weight, ind_type='zx'):
     return industry_dat
 
 def get_trade_date(date):
-    month_map = pd.read_excel(wdir, 'quote_data', 'month_map.xlsx')
-    month_map = month_map.applymap(pd.to_datetime)
-    month_map = month_map.set_index(['calendar_date'])
-    date = month_map.loc[date][0]
+    date = DateUtil.add_date(date, '.ap')
     return date
 
 def get_panel_data(date, panel_dir):
     dat = pd.read_csv(os.path.join(panel_dir, f'{str(date)[:10]}.csv'), encoding='gbk',
                       engine='python', index_col=[0])
     return dat
-    
-def get_stocks_ret(weight, freq='M'):    
+
+def get_stocks_ret(weight, freq='M'):
     wt = weight.copy()
     if freq.endswith('M'):
         fname = f'pct_chg_{freq}.csv'
@@ -42,11 +58,11 @@ def get_stocks_ret(weight, freq='M'):
         fname = 'pct_chg.csv'
     else:
         raise RuntimeError("Unsupported return Type!")
-        
+
     pct_chg = pd.read_csv(os.path.join(wdir, 'quote_data', fname),
                           engine='c', index_col=[0], encoding='gbk')
     pct_chg.columns = pd.to_datetime(pct_chg.columns)
-    
+
     if freq.endswith('M'):
         dates = [get_cdate(date) for date in weight.columns]
         pct_chg = pct_chg.loc[weight.index, dates]
@@ -67,58 +83,58 @@ def cal_ind_ret_weight(weight, freq='6M'):
     dates = weight.columns.tolist()
     ind_dat = get_ind_data(weight)
     ret_dat = get_stocks_ret(weight, freq)
-        
+
     ind_return = []; ind_weight = []
-    for date in dates: 
+    for date in dates:
         if freq.endswith('M'):
             cur_stk = weight[date].dropna().index
             cur_ind = ind_dat.loc[cur_stk, date]
             cur_ret = ret_dat.loc[cur_stk, date]
             cur_weight = weight.loc[cur_stk, date]
-                    
+
             cur_datdf = pd.concat([cur_ind, cur_ret, cur_weight], axis=1)
             cur_datdf.columns = ['industry', 'return', 'weight']
-            
+
             cur_ind_ret = cur_datdf.groupby(['industry']).apply(cal_group_ret)
             cur_ind_weight = cur_datdf.groupby(['industry']).apply(lambda df: df['weight'].sum())
         cur_ind_ret.name = cur_ind_weight.name = date
-        
+
         ind_return.append(cur_ind_ret)
         ind_weight.append(cur_ind_weight)
-        
+
     ind_return = pd.DataFrame(ind_return).fillna(0)
     ind_weight = pd.DataFrame(ind_weight).T.fillna(0)
     ind_weight /= ind_weight.sum()
-    return ind_return, ind_weight.T    
+    return ind_return, ind_weight.T
 
-def brinson_attr_asset(stock_weight, asset_weight, fund_code, stock_bm='000300.SH', 
+def brinson_attr_asset(stock_weight, asset_weight, fund_code, stock_bm='000300.SH',
                        bond_bm='000012.SH', freq='6M', version=2, verbose=False):
-    brinson_stock = brinson_attr_stock(stock_weight, stock_bm, freq, version, 
+    brinson_stock = brinson_attr_stock(stock_weight, stock_bm, freq, version,
                                        verbose, fund_code)
     brinson_stock.index = [get_cdate(date) for date in brinson_stock.index]
-    
+
     bond_bm_ret = get_index_ret(bond_bm, freq)
     bond_bm_ret = bond_bm_ret.loc[brinson_stock.index]
-    
+
     stock_bm_ret = get_index_ret(stock_bm, freq)
     stock_bm_ret = stock_bm_ret.loc[brinson_stock.index]
-    
+
 #    bm_ret = brinson_stock['re_b'] * asset_weight['bm_stock'] + bond_bm_ret * asset_weight['bm_bond']
     bm_ret = stock_bm_ret * asset_weight['bm_stock'] + bond_bm_ret * asset_weight['bm_bond']
-    
+
     fund_ret = get_index_ret(fund_code, freq)
     fund_ret = fund_ret.loc[brinson_stock.index]
-    
+
     timing_ret = (asset_weight['pt_stock'] - asset_weight['bm_stock']) * (brinson_stock['re_b'] - bm_ret) + \
                  (asset_weight['pt_bond'] - asset_weight['bm_bond']) * (bond_bm_ret - bm_ret)
     ind_ret = brinson_stock['配置效应(AR)'] * asset_weight['pt_stock']
     select_ret = brinson_stock['选股效应(SR)'] * asset_weight['pt_stock']
-    
+
     res_con_timing = pd.concat([fund_ret, bm_ret, timing_ret, ind_ret, select_ret], axis=1)
     res_con_timing.columns = ['基金收益', '基准实际收益', '大类资产择时收益(TR)', '配置效应(AR)', '选股效应(SR)']
     res_con_timing['估计误差'] = res_con_timing['基金收益'] - res_con_timing[['基准实际收益', '大类资产择时收益(TR)', '配置效应(AR)', '选股效应(SR)']].sum(axis=1)
     res_con_timing['是否调整'] = '调整后'
-    
+
     res_wo_timing = pd.concat([fund_ret, bm_ret, brinson_stock[['配置效应(AR)', '选股效应(SR)']]], axis=1)
     res_wo_timing.columns = ['基金收益', '基准实际收益', '配置效应(AR)', '选股效应(SR)']
     res_wo_timing['大类资产择时收益(TR)'] = np.nan
@@ -134,7 +150,7 @@ def brinson_attr_asset(stock_weight, asset_weight, fund_code, stock_bm='000300.S
                '选股效应(SR)', '配置效应(AR)', '估计误差']]
     return res
 
-def brinson_attr_stock(weight, benchmark='000300.SH', freq='6M', version=2, 
+def brinson_attr_stock(weight, benchmark='000300.SH', freq='6M', version=2,
                        verbose=False, fund_code=None):
     r1 = ['配置效应(AR)','选股效应(SR)','交互效应(IR)']
     r2 = ['配置效应(AR)','选股效应(SR)']
@@ -144,7 +160,7 @@ def brinson_attr_stock(weight, benchmark='000300.SH', freq='6M', version=2,
     bm_weight.columns = pd.to_datetime(bm_weight.columns)
     bm_weight = bm_weight[weight.columns]
     bm_weight /= bm_weight.sum()
-        
+
     pt_ind_ret, pt_ind_weight = cal_ind_ret_weight(weight)
     bm_ind_ret, bm_ind_weight = cal_ind_ret_weight(bm_weight)
     bm_ind_weight.iloc[0]
@@ -155,31 +171,37 @@ def brinson_attr_stock(weight, benchmark='000300.SH', freq='6M', version=2,
         for col in cols:
             pt_ind_weight.loc[:, col] = 0
             pt_ind_ret.loc[:, col] = 0
-            
+
     if len(mut_ind.difference(bm_ind_weight.columns)) > 0:
         cols = mut_ind.difference(bm_ind_weight.columns)
         for col in cols:
             bm_ind_weight.loc[:, col] = 0
             bm_ind_ret.loc[:, col] = 0
-    
-    brinson_single = brinson_attr_single_period(pt_ind_ret, pt_ind_weight, 
+
+    brinson_single = brinson_attr_single_period(pt_ind_ret, pt_ind_weight,
                                     bm_ind_ret, bm_ind_weight, version)
     if verbose:
         if fund_code is None:
             raise RuntimeError('Need to pass in "fund_code" to save attribution result file!')
-        brinson_single.to_excel(os.path.join(wdir, 'brinson_result',
-                                f'{fund_code}_res.xlsx'), encoding='gbk')
+        writer = pd.ExcelWriter(os.path.join(wdir, 'brinson_result_test',
+                                f'{fund_code}_res.xlsx'))
+        for i in brinson_single.keys():
+            brinson_single[i].to_excel(writer, sheet_name=i)
+        writer.save()
+        # brinson_single.to_excel(os.path.join(wdir, 'brinson_result',
+        #                         f'{fund_code}_res.xlsx'), encoding='gbk')
     brinson_returns = r1 if version == 1 else r2
     single_ret = pd.DataFrame()
     for r in brinson_returns:
-        dat_panel = brinson_single.minor_xs(r)
-        if '总计' in dat_panel.index:
-            dat_panel.drop(['总计'], inplace=True)
-        single_ret[r] = dat_panel.sum(axis=0)
-    
+        for j in brinson_single.keys():
+            dat_panel = brinson_single[j][r]
+            if '总计' in dat_panel.index:
+                dat_panel.drop(['总计'], inplace=True)
+            single_ret.loc[j, r] = dat_panel.sum(axis=0)
+
     port_ret = (pt_ind_ret * pt_ind_weight).sum(axis=1)
     bm_ret = (bm_ind_ret * bm_ind_weight).sum(axis=1)
-    
+
     single_ret.index = pd.to_datetime(single_ret.index)
     single_ret = pd.concat([single_ret, port_ret, bm_ret], axis=1)
     single_ret.columns = brinson_returns + ['re_p', 're_b']
@@ -188,7 +210,7 @@ def brinson_attr_stock(weight, benchmark='000300.SH', freq='6M', version=2,
 def brinson_attr_single_period(pt_ret, pt_weight, bm_ret, bm_weight, version=2):
     result = {}
     bm_total_ret = (bm_ret * bm_weight).sum(axis=1)
-    for date in pt_ret.index: 
+    for date in pt_ret.index:
         res = pd.DataFrame(index=bm_weight.columns)
         res['组合权重'] = pt_weight.loc[date]
         res['基准权重'] = bm_weight.loc[date]
@@ -205,7 +227,7 @@ def brinson_attr_single_period(pt_ret, pt_weight, bm_ret, bm_weight, version=2):
         res.loc['总计'] = res.sum()
         res.loc['总计', ['组合收益', '基准收益']] = np.nan
         result[str(date)[:10]] = res
-    result = pd.Panel(result)
+    # result = pd.Panel(result)
     return result
 
 def get_cdate(date):
@@ -217,14 +239,14 @@ def get_cdate(date):
     return cdate
 
 def get_index_ret(code, freq='6M'):
-	"""
-		将日收益率转换为设定频率的收益率。
-		例如，freq默认为6个月时，将日收益率转换为半年度收益，且默认计算时间范围为
-		1-6月及7-12月，计算起始日期选择基金或者指数成立后的首个完整的半年度的首个
-		月份第1个交易日（1月或者7月）
+    """
+    将日收益率转换为设定频率的收益率。
+    例如，freq默认为6个月时，将日收益率转换为半年度收益，且默认计算时间范围为
+    1-6月及7-12月，计算起始日期选择基金或者指数成立后的首个完整的半年度的首个
+    月份第1个交易日（1月或者7月）
 	"""
     ret = pd.read_csv(os.path.join(wdir, 'quote_data', f'{code}.csv'), parse_dates=True,
-                           engine='python', encoding='gbk', index_col=[0])
+                      engine='python', encoding='gbk', index_col=[0])
     ret = ret.dropna(how='any', axis=0)['pct_change']
     if freq.endswith('M') and freq != 'M':
         num_months = int(freq[:-1])
@@ -244,13 +266,13 @@ def get_index_ret(code, freq='6M'):
 
 def clean_index_quote(save_cols=('close',), save_ori=False):
     """
-       input: 
+       input:
 	   从wind终端下载的基金/指数日频行情数据文件，文件名格式：'基金代码'.xls
        output:
            根据close_price计算日收益率, 根据save_cols参数决定所要原始的数据列；
 	   结果存储为csv，通过save_ori关键字参数决定是否保留原始xls文件，默认值为False
 	   存储结果见quote_data文件夹
-    """		    
+    """
     quote_dir = os.path.join(wdir, 'quote_data')
     files = [f for f in os.listdir(quote_dir) if f.endswith('xls')]
     col_map = {
@@ -263,11 +285,11 @@ def clean_index_quote(save_cols=('close',), save_ori=False):
             'amount': '成交额(百万)',
             'volume': '成交量(股)',
             }
-    
-    save_map = {col_map[col.lower()]:col.lower() for col in save_cols 
+
+    save_map = {col_map[col.lower()]:col.lower() for col in save_cols
                 if col.lower() in col_map.keys()}
-    
-    for f in files: 
+
+    for f in files:
         dat = pd.read_excel(os.path.join(quote_dir, f), encoding='gbk')
         dat = dat.dropna(how='any', axis=0)
         dat['pct_change'] = dat[['收盘价(元)']].pct_change()
@@ -281,7 +303,7 @@ def clean_index_quote(save_cols=('close',), save_ori=False):
 
 def clean_fund_holding(save_ori=True):
     """
-       input: 
+       input:
 	   从wind终端下载的基金持仓明细文件，文件名格式：'基金代码'持股.csv
        output:
 	   结果存储为xlsx，每个sheet名为对应持仓报告期日期，
@@ -290,36 +312,44 @@ def clean_fund_holding(save_ori=True):
     """
     fund_dir = os.path.join(wdir, 'fund_holding')
     files = [f for f in os.listdir(fund_dir) if '持股' in f]
-    for f in files: 
+    for f in files:
         try:
-            dat = pd.read_csv(os.path.join(fund_dir, f), engine='c', 
+            dat = pd.read_csv(os.path.join(fund_dir, f),
                               encoding='utf-8', index_col=[0], parse_dates=True)
         except UnicodeDecodeError:
-            dat = pd.read_csv(os.path.join(fund_dir, f), engine='c', 
-                              encoding='gbk', index_col=[0], parse_dates=True)
-  
+            dat = pd.read_csv(os.path.join(fund_dir, f),
+                              encoding='gbk-8', index_col=[0], parse_dates=True)
+
+        # dat = pd.read_csv(os.path.join(fund_dir, f), engine='c',
+        #                   encoding='utf-8', index_col=[0], parse_dates=True)
         dat = dat.reset_index()
         dat['报告期'] = dat['报告期'].map(lambda d: str(d)[:10])
+        dates = list(set(dat['报告期'].tolist()))
         del dat['序号']
-        dat = dat.set_index(['品种代码', '报告期'])
-        dat = dat.to_panel()
-        dat = dat.swapaxes(0, 2)
-        dat.to_excel(os.path.join(fund_dir, f.split('持股')[0]+'.xlsx'),
-                     encoding='gbk')
+        dat = dat.set_index([ '报告期'])
+        writer = pd.ExcelWriter(os.path.join(fund_dir, f.split('持股')[0]+'.xlsx'))
+        for i in dates:
+            dat.loc[i, :].reset_index(drop=True).to_excel(writer, sheet_name=i, index=False)
+        writer.save()
+        # dat = dat.swaplevel(0, 1).to_dict()
+        # dat.to_dict()
+        # dat = {'1': pd.DataFrame(), '2': pd.DataFrame()}
+        # dat = dat.swapaxes(0, 1)
+        # dat.to_excel(os.path.join(fund_dir, f.split('持股')[0]+'.xlsx'),
+        #              encoding='gbk')
         if not save_ori:
             os.remove(os.path.join(fund_dir, f))
 
 def read_fund_holding(code, index=None, bm_stock_wt=0.80):
     fund_dir = os.path.join(wdir, 'fund_holding')
-    dat = pd.read_excel(os.path.join(fund_dir, code+'.xlsx'), encoding='gbk', 
-                        sheet_name=None)
+    dat = pd.read_excel(os.path.join(fund_dir, code+'.xlsx'), sheet_name=None)
     if index:
         index_weight = pd.read_csv(os.path.join(wdir, 'index_weight', f'{index}.csv'),
                          engine='python', encoding='gbk', index_col=[0])
         index_weight.columns = pd.to_datetime(index_weight.columns)
-        
+
     stock_weight = pd.DataFrame(); asset_weight = pd.DataFrame(columns=['stock', 'bond'])
-    for date in dat.keys(): 
+    for date in dat.keys():
         panel = dat[date]
         date = pd.to_datetime(date)
         panel = panel[panel['所属行业名称'].notnull()]
@@ -331,12 +361,13 @@ def read_fund_holding(code, index=None, bm_stock_wt=0.80):
         panel = panel.set_index(['code'])
         stock_weight = pd.concat([stock_weight, panel['weight']], axis=1)
         asset_weight.loc[date] = [panel['占基金净值比(%)'].sum()/100, 1 - panel['占基金净值比(%)'].sum()/100]
-    
+
     asset_weight.columns = ['pt_stock', 'pt_bond']
     asset_weight['bm_stock'] = bm_stock_wt
     asset_weight['bm_bond'] = 1 - bm_stock_wt
     stock_weight.columns = [get_trade_date(pd.to_datetime(date)) for date in dat.keys()]
     return stock_weight, asset_weight
+
 
 def brinson_attribution():
     fund_code = '161810.OF'        #基金代码
@@ -346,18 +377,18 @@ def brinson_attribution():
     version = 2               #brinson归因模型版本 1--BHB; 2--BF
     freq = '6M'               #归因频率，与所选基金持仓频率对应，默认选择基金的半年报和年报
     verbose = True        #是否存储单层brinson归因结果（股票行业配置和选股效应）
-    
+
     clean_index_quote()    #清洗并计算基金/基准指数日收益率
     clean_fund_holding()   #清洗基金持仓文件
-    stock_weight, asset_weight = read_fund_holding(fund_code, None, bm_stock_wt) 
-    res = brinson_attr_asset(stock_weight, asset_weight, fund_code, stock_benchmark, 
+    stock_weight, asset_weight = read_fund_holding(fund_code, None, bm_stock_wt)
+    res = brinson_attr_asset(stock_weight, asset_weight, fund_code, stock_benchmark,
                              bond_benchmark, freq, version, verbose)
-			    
+
     if not os.path.exists(os.path.join(wdir, 'brinson_result')):
-	os.mkdir(os.path.join(wdir, 'brinson_result'))
-    res.to_csv(os.path.join(wdir, 'brinson_result', f'{fund_code}.csv'), 
+        os.mkdir(os.path.join(wdir, 'brinson_result'))
+    res.to_csv(os.path.join(wdir, 'brinson_result', f'{fund_code}.csv'),
                encoding='gbk')
     print(f'Finish for {fund_code}.')
-    
+
 if __name__ == '__main__':
     brinson_attribution()
